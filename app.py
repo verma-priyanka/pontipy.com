efd2e0276ded6842d1e86af3295a2e39f3526926
verma-priyanka <pverm157@gmail.com>
2021-03-23 15:58:15 -0400

    table

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+
+df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                   'B': [5, 6, 7, 8, 9],
+                   'C': ['a', 'b', 'c--', 'd', 'e']})
+
+@app.route('/test')
+def html_table():
+    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 @app.route("/")
 def home():
