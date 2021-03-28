d08de181559422f442c971af710d48a16160e4c8
verma-priyanka <pverm157@gmail.com>
2021-03-27 21:55:53 -0400

    built with bootstrap

--- app.py
+++ app.py
@@ -4,23 +4,37 @@ import pandas as pd
 app = Flask(__name__)
 
 
-df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
-                   'B': [5, 6, 7, 8, 9],
-                   'C': ['a', 'b', 'c--', 'd', 'e']})
+df = pd.DataFrame({'1985': [10, 20, 30, 3, 4],
+                   '1986': [5, 6, 7, 8, 9],
+                   '1987': ['a', 'b', 'c', 'd', 'e'],
+                   '1988': ['a', 'b', 'c', 'd', 'e'],
+                   '1989': ['a', 'b', 'c', 'd', 'e'],
+                   '1990': ['a', 'b', 'c', 'd', 'e'],
+                   '1991': [5, 6, 7, 8, 9],
+                   '1992': [5, 6, 7, 8, 9],
+                   '1993': [5, 6, 7, 8, 9],
+                   '1994': [5, 6, 7, 8, 9],
+                   '1995': [5, 6, 7, 8, 9]})
 
-@app.route('/test')
-def html_table():
-    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-# add a new page
+@app.route('/tabletest')
+def html_table():
+    return render_template('tabletest.html',  tables=[df.to_html(classes='table table-bordered table-sm')], titles="")
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/help")
+def help():
+    return render_template("help.html")
 
 # @app.route("/upload-csv")
 # def upload_csv():
